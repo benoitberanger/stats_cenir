380c23fa60bc965e3b9a3149e6a19dc03fc617ce
Benoît Béranger <benoit.beranger@icm-institute.org>
2016-12-01 14:08:07 +0100

    new method to count NrScan & TimeScan : using cell instead of struct todo : modify this method so we can enter any table, then we could use a specific table, based on 1 protocol to get the same data !

--- script1_ImportParseCompute.m
+++ script1_ImportParseCompute.m
@@ -260,6 +260,114 @@ t.e.txt(:,col.e.duration) = cell(size(diff_time));
 t.e.raw(:,col.e.duration) = num2cell(diff_time);
 
 
+%% Prepare time
+
+counter = 0;
+current_dateVect = datevec(now);
+time = struct;
+for yyyy = 2010:current_dateVect(1)
+    for mm = 1:12
+        counter = counter + 1;
+        time.vect(counter,:) = [ yyyy mm 1 0 0 0 ];
+    end
+end
+time.unix = datenum_to_unixtime( datenum(time.vect) );
+time.unix_plus_one = datenum_to_unixtime(datenum([current_dateVect(1)+1 1 1 0 0 0 ]));
+
+
+%% Prepare indexs
+
+for l = 1 : length(list)
+    X = list{l};
+    
+    index.(X).prisma = t.(X).num(:,col.(X).room_id) == 1 ;
+    index.(X).verio  = t.(X).num(:,col.(X).room_id) == 19;
+end
+index.a.auto = strcmp(t.a.txt(:,col.a.del_by) , 'auto');
+index.a.m10  = ( t.a.num(:,col.a.delay_time_day) < md10 ) & ~index.a.auto;
+index.a.p10  = ( t.a.num(:,col.a.delay_time_day) > pd10 ) & ~index.a.auto;
+
+
+%% Prepare cell container for everything
+
+names.res = {'month_idx' 'year' 'unix' 'prisma_e' 'prisma_auto' 'prisma_m10' 'prisma_p10' 'verio_e' 'verio_auto' 'verio_m10' 'verio_p10' };
+
+nCol.res = 0;
+for n = 1:length(names.res)
+    nCol.res = nCol.res +1;
+    col.res.( names.res {n} ) = nCol.res;
+end
+
+hdr.res = update_hdr(col.res);
+
+
+%% Fill the reuslt cell
+
+res = cell(length(time.unix),nCol.res);
+
+res(:,col.res.month_idx) = num2cell(1:length(time.unix));
+res(:,col.res.year) = num2cell(time.vect(:,1));
+res(:,col.res.unix) = num2cell(time.unix);
+
+N = nan(length(time.unix),nCol.res);
+
+N(:,col.res.month_idx) = cell2mat(res(:,col.res.month_idx));
+N(:,col.res.year) = cell2mat(res(:,col.res.year));
+N(:,col.res.unix) = cell2mat(res(:,col.res.unix));
+
+T = N;
+
+for mm = 1 : length(time.unix)
+    
+    timeinf = time.unix(mm);
+    if mm ~= length(time.unix)
+        timesup = time.unix(mm+1);
+    else
+        timesup = time.unix_plus_one;
+    end
+    
+    current_month_idx_e = ( t.e.num(:,col.e.start_time) >= timeinf ) & ( t.e.num(:,col.e.start_time) < timesup );
+    
+    res{mm,col.res.prisma_e} = find( current_month_idx_e & index.e.prisma );
+    N  (mm,col.res.prisma_e) = length(res{mm,col.res.prisma_e});
+    T  (mm,col.res.prisma_e) = sum(t.e.num( res{mm,col.res.prisma_e} , col.e.duration ) );
+    
+    res{mm,col.res.verio_e } = find( current_month_idx_e & index.e.verio  );
+    N  (mm,col.res.verio_e ) = length(res{mm,col.res.verio_e });
+    T  (mm,col.res.verio_e ) = sum(t.e.num( res{mm,col.res.verio_e } , col.e.duration ) );
+    
+    
+    current_month_idx_a = ( t.a.num(:,col.a.start_time) >= timeinf ) & ( t.a.num(:,col.a.start_time) < timesup );
+    
+    res{mm,col.res.prisma_auto} = find( current_month_idx_a & index.a.prisma & index.a.auto );
+    N  (mm,col.res.prisma_auto) = length(res{mm,col.res.prisma_auto});
+    T  (mm,col.res.prisma_auto) = sum(t.a.num( res{mm,col.res.prisma_auto} , col.a.duration ) );
+    
+    res{mm,col.res.prisma_m10 } = find( current_month_idx_a & index.a.prisma & index.a.m10  );
+    N  (mm,col.res.prisma_m10) = length(res{mm,col.res.prisma_m10});
+    T  (mm,col.res.prisma_m10) = sum(t.a.num( res{mm,col.res.prisma_m10} , col.a.duration ) );
+    
+    res{mm,col.res.prisma_p10 } = find( current_month_idx_a & index.a.prisma & index.a.p10  );
+    N  (mm,col.res.prisma_p10) = length(res{mm,col.res.prisma_p10});
+    T  (mm,col.res.prisma_p10) = sum(t.a.num( res{mm,col.res.prisma_p10} , col.a.duration ) );
+    
+    res{mm,col.res.verio_auto } = find( current_month_idx_a & index.a.verio  & index.a.auto );
+    N  (mm,col.res.verio_auto) = length(res{mm,col.res.verio_auto});
+    T  (mm,col.res.verio_auto) = sum(t.a.num( res{mm,col.res.verio_auto} , col.a.duration ) );
+    
+    res{mm,col.res.verio_m10  } = find( current_month_idx_a & index.a.verio  & index.a.m10  );
+    N  (mm,col.res.verio_m10) = length(res{mm,col.res.verio_m10});
+    T  (mm,col.res.verio_m10) = sum(t.a.num( res{mm,col.res.verio_m10} , col.a.duration ) );
+    
+    res{mm,col.res.verio_p10  } = find( current_month_idx_a & index.a.verio  & index.a.p10  );
+    N  (mm,col.res.verio_p10) = length(res{mm,col.res.verio_p10});
+    T  (mm,col.res.verio_p10) = sum(t.a.num( res{mm,col.res.verio_p10} , col.a.duration ) );
+    
+end
+
+
+return
+
 %% Prepare year splitting
 
 s = struct;
@@ -373,7 +481,16 @@ for y = 1 : size(s.str,1) - 1
     end
     
     %     s.year.(year).table.(X).a_m10.idx = s.year.(year).table.(X).idx
-    
+%     for m = 1:12
+%         month = s.year.(year).str(m,:);
+%         if m==12
+%             s.year.(year).month.(month).table.(X).idx = find( and( t.(X).num(:,col.(X).start_time) >= s.year.(year).unix(m) , t.(X).num(:,col.(X).start_time) < s.unix(y+1) ) );
+%         else
+%             s.year.(year).month.(month).table.(X).idx = find( and( t.(X).num(:,col.(X).start_time) >= s.year.(year).unix(m) , t.(X).num(:,col.(X).start_time) < s.year.(year).unix(m+1) ) );
+%         end
+%         s.year.(year).month.(month).table.(X).N = length(s.year.(year).month.(month).table.(X).idx);
+%         s.year.(year).month.(month).table.(X).t = sum( t.(X).num( s.year.(year).month.(month).table.(X).idx , col.(X).duration ) );
+%     end
     
 end
 
